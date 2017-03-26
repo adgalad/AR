08cc0df2b88f9f69ca4a229613323f81a8d2e616
adgalad <carlos.25896@gmail.com>
2017-03-26 01:14:58 -0400

    arreglando cosas

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-	public Material defaultMaterial, material;
 	public GameObject start;
 
+	public float startSpeed;
 	WebCamTexture webCamTexture;
 	WebCamDevice[] devices;
 	Renderer rend;
@@ -29,6 +29,12 @@ public class Ball : MonoBehaviour {
 	
 		rb.AddForce (move*100);
 
+		if (rb.velocity.magnitude > 10 && rb.velocity.magnitude < 40) {
+			rb.AddForce (rb.velocity.normalized * 40);
+		} else if (rb.velocity.magnitude >= 40) {
+			rb.AddForce (new Vector3 (-1, 0, 0) * 10);
+		}
+
 	}
 	void OnTriggerEnter(Collider other){
 
@@ -40,9 +46,25 @@ public class Ball : MonoBehaviour {
 		}
 	}
 
+
+
+	IEnumerator wait(int secs) {
+		
+		yield return new WaitForSecondsRealtime(secs);
+		Vector3 direction = new Vector3 (1, 0, Random.Range (-1, 1));
+		print (direction);
+		rb.velocity =  (direction * startSpeed);
+
+	}
+
 	void reset () {
 		rb.velocity        = Vector3.zero;
 		transform.position = start.transform.position;
+		transform.rotation = new Quaternion (0, 0, 0, 0);
+
+		StartCoroutine(wait (2));
+
+
 	}
 
 }
---
+++ Assets/Brick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public Material[] materials;
+
+	private Renderer rend;
+	private int currentDamage = 0;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent <Renderer> ();
+		rend.material = materials [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Ball")
+		{
+			Debug.Log (materials.Length);
+			++currentDamage;
+			if (currentDamage >= materials.Length) {
+				gameObject.SetActive (false);
+			} else {
+				rend.material = materials [currentDamage];
+			}
+				
+
+		}
+	}
+}
